d776d6639ad3d7016bd5c302a70ff1954687165c
Ivan-KaGG <54744462+Ivan-KaGG@users.noreply.github.com>
2020-04-10 17:27:20 -0300

    assured

--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class ItemBehaviour : MonoBehaviour
 {
     public int randomic;
+    private Collider myCollider;
     public GameObject instantiated;
     public GameObject myPrefabUsable;
     public GameObject myPrefabUnusable;
 
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+    }
     void OnMouseDown()
     {
         randomic = Random.Range(0,2);
+        Debug.Log(randomic);
 
         if(randomic == 0)
         {
@@ -24,18 +30,28 @@ public class ItemBehaviour : MonoBehaviour
         if(gameObject.tag=="usable")
         {
             Debug.Log("Usable");
-            Destroy(gameObject);
-            Instantiate(instantiated, new Vector3(-2.7f,-2.2f,0), Quaternion.identity);
-            
+
         }else if(gameObject.tag=="unusable")
         {
             Debug.Log("<color=red>Unusable</color>");
+        }
+
+        if(gameObject.transform.position.x== -2.7f)
+        {
+            Destroy(gameObject);
+            Instantiate(instantiated, new Vector3(-2.7f,-2.2f,0), Quaternion.identity);
+            
+        }else if(gameObject.transform.position.x== -1.7f)
+        {
             Destroy(gameObject);
             Instantiate(instantiated, new Vector3(-1.7f,-2.2f,0), Quaternion.identity);
             
-        }
+        }   
 
-        Debug.Log(randomic);
+        if(!myCollider.enabled)
+        {
+            Debug.Log("isn't enabled");
+        }             
         
     }
 }
